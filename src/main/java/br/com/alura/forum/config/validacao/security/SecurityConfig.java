48ba90577f7bc721f8673f572bd7031a642a69b4
Diego Ferreira <diego.ferreira@justa.com.vc>
2020-07-06 21:53:27 -0300

    feat: :lock: Adiciona autenticação com spring security e token jwt
    Signed-off-by: Diego Ferreira <diego.ferreira@justa.com.vc>

--- src/main/java/br/com/alura/forum/config/validacao/security/SecurityConfig.java
+++ src/main/java/br/com/alura/forum/config/validacao/security/SecurityConfig.java
@@ -1,16 +1,31 @@
 package br.com.alura.forum.config.validacao.security;
 
+import br.com.alura.forum.services.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
     /*
     Configurações de autorização
@@ -19,8 +34,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin();
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     /*
@@ -28,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(authenticationService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     /*
---
+++ src/main/java/br/com/alura/forum/services/AuthenticationService.java
@@ -0,0 +1,28 @@
+package br.com.alura.forum.services;
+
+import br.com.alura.forum.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    private br.com.alura.forum.repository.UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Usuario> usuario = repository.findByEmail(username);
+        if (usuario.isPresent()) {
+            return usuario.get();
+        }
+
+        throw new UsernameNotFoundException("Dados inválidos!");
+    }
+
+}
